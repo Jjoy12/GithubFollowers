6e04cad8a6f2e761a3d17171bbf5f7e7f3ccf9e7
Jjoy12 <Jerrionjoy@gmail.com>
2020-12-26 17:48:44 -0500

    Initial Setup

---
+++ GithubFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubFollowers
+//
+//  Created by Jerrion Joy on 12/26/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
