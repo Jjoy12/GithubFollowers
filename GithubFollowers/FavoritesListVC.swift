0780883d6f8047646a9e599cb96e97ce78d84c70
Jjoy12 <Jerrionjoy@gmail.com>
2020-12-26 17:44:55 -0500

    Initial Setup

---
+++ GithubFollowers/FavoritesListVC.swift
@@ -0,0 +1,20 @@
+//
+//  FavoritesListVC.swift
+//  GithubFollowers
+//
+//  Created by Jerrion Joy on 12/26/20.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBlue
+    }
+    
+
+   
+}
