6d040950e211dd69db8f97de58e4d7cac07b1058
Jjoy12 <Jerrionjoy@gmail.com>
2020-12-26 17:41:09 -0500

    Initial Setup

---
+++ GithubFollowers/GFTextField.swift
@@ -0,0 +1,20 @@
+//
+//  GFTextField.swift
+//  GithubFollowers
+//
+//  Created by Jerrion Joy on 12/26/20.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
