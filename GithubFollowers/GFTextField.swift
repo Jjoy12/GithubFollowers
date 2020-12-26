7aa128edebd05ceba2b1cf0e7d41e10364245c72
Jjoy12 <Jerrionjoy@gmail.com>
2020-12-26 17:52:19 -0500

    Custom textfield

--- GithubFollowers/GFTextField.swift
+++ GithubFollowers/GFTextField.swift
@@ -8,13 +8,37 @@
 import UIKit
 
 class GFTextField: UITextField {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
+// Custom text field configuration
+    private func configure() {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        
+        backgroundColor = .tertiarySystemBackground
+        keyboardType = .emailAddress
+        returnKeyType = .go
+        autocorrectionType = .no
+        placeholder = "Enter a username"
+        
+    }
 }
