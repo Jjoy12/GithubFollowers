146ea192168ca42b72e2f979734dab059b986e8a
Jjoy12 <Jerrionjoy@gmail.com>
2020-12-27 09:07:13 -0500

    Followers List view controller initial Set up

---
+++ GithubFollowers/FollowerListVC.swift
@@ -0,0 +1,25 @@
+//
+//  FollowerListVC.swift
+//  GithubFollowers
+//
+//  Created by Jerrion Joy on 12/26/20.
+//
+
+import UIKit
+
+class FollowerListVC: UIViewController {
+    
+    var username: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+        navigationController?.isNavigationBarHidden = false
+        navigationController?.navigationBar.prefersLargeTitles = true 
+        
+    }
+    
+    
+
+}
