5539fbe10701c8768703cc135a65228713de1d60
Jjoy12 <Jerrionjoy@gmail.com>
2020-12-27 21:42:25 -0600

    presenting custom alert modaly through extension

--- GithubFollowers/UIViewController+ext.swift
+++ GithubFollowers/UIViewController+ext.swift
@@ -5,4 +5,16 @@
 //  Created by Jerrion Joy on 12/27/20.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController{
+    
+    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String){
+        DispatchQueue.main.async {
+            let alertVC = GFAlertVC(title: title, message: message, buttonTitle: buttonTitle)
+            alertVC.modalPresentationStyle = .overFullScreen
+            alertVC.modalTransitionStyle = .flipHorizontal
+            self.present(alertVC, animated: true)
+        }
+    }
+}
