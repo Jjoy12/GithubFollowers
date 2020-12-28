01bb2538e6247f6f3b839c958faee1829b5afd14
Jjoy12 <Jerrionjoy@gmail.com>
2020-12-27 21:38:34 -0600

    custom title label

--- GithubFollowers/GFTitleLabel.swift
+++ GithubFollowers/GFTitleLabel.swift
@@ -8,13 +8,33 @@
 import UIKit
 
 class GFTitleLabel: UILabel {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureTitleLabel()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(textAlignment: NSTextAlignment, fontSize: CGFloat) {
+        
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        self.font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
+        configureTitleLabel()
+        
+    }
+    
+    private func configureTitleLabel(){
+        textColor = .label
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.9
+        textAlignment = .center
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+        
+    }
 }
