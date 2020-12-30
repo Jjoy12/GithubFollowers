2289d808e07e3c256b5e6147e1c5f0e6e57728fa
Jjoy12 <Jerrionjoy@gmail.com>
2020-12-30 00:17:30 -0600

    refactor

--- GithubFollowers/Manager/NetworkManager.swift
+++ GithubFollowers/Manager/NetworkManager.swift
@@ -16,35 +16,35 @@ class NetworkManager {
     private init() {}
     
 //MARK: Get followers function
-    func getfollowers(for username: String, page: Int, completed: @escaping ([Follower]?, String?) -> Void) {
+    func getfollowers(for username: String, page: Int, completed: @escaping (Result<[Follower], GFError>) -> Void) {
         let endpoint = baseURL +  "\(username)/followers?per_page=100&page=\(page)"
         
         guard let url = URL(string: endpoint) else {
-            completed(nil,"This username created an invalid request please try again")
+            completed(.failure(.invalidUsername))
             return
         }
         
         let task = URLSession.shared.dataTask(with: url) {data, response, error in
             if let _ = error {
-               completed(nil,"Unable to complete your request please check your internet connection")
+                completed(.failure(.unableToComplete))
             }
             
             guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                completed(nil,"Invalid response from the server please try again")
+                completed(.failure(.unableToComplete))
                 return
             }
             
             guard let data = data else {
-                completed(nil,"The data receieved from the server was invalid Please try again")
+                completed(.failure(.invalidData))
                 return
             }
             do {
                 let decoder = JSONDecoder()
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
                 let followers = try decoder.decode([Follower].self, from: data)
-                completed(followers,nil)
+                completed(.success(followers))
             }catch {
-                completed(nil,"The data receieved from the server was invalid Please try again")
+                completed(.failure(.invalidData))
                 
             }
             
