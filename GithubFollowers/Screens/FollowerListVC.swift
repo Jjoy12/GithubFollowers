a3a2595b73e3c83727e7c544a948d3ee9b9ddbe2
Jjoy12 <Jerrionjoy@gmail.com>
2021-01-06 09:00:17 -0600

    Adding dimiss and empty state to followers vc

--- GithubFollowers/Screens/FollowerListVC.swift
+++ GithubFollowers/Screens/FollowerListVC.swift
@@ -22,28 +22,28 @@ class FollowerListVC: UIViewController {
     var collectionView: UICollectionView!
     var dataSource: UICollectionViewDiffableDataSource<Section,Follower>!
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureViewController()
         configureCollectionView()
         getFollowers(username: username, page: page)
         configureDataSource()
-    
+        
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.isNavigationBarHidden = false
+        
+    }
     
-        }
     
-   
     func configureViewController() {
         view.backgroundColor = .systemBackground
         navigationController?.isNavigationBarHidden = false
         navigationController?.navigationBar.prefersLargeTitles = true
-
-            }
+        
+    }
     
     
     func configureCollectionView() {
@@ -54,28 +54,35 @@ class FollowerListVC: UIViewController {
     }
     
     
-        
+    
     
     func getFollowers(username: String, page: Int) {
-//MARK: Network Call
-           NetworkManager.shared.getfollowers(for: username, page: 1) {[weak self] result in
+        showLoadingView()
+        
+        //MARK: Network Call
+        NetworkManager.shared.getfollowers(for: username, page: 1) {[weak self] result in
             
             guard let self = self else {return}
+            self.dismissLoadingView()
             
-                      
-                      switch result {
-                      case .success(let followers):
-                        if followers.count < 100 {self.hasMoreFollowers = false}
-                        self.followers.append(contentsOf: followers)
-                        self.updateData()
-                      
-                      case .failure(let error):
-                          self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "Ok")
-                      }
-                    
-                  }
-                  
-              }
+            switch result {
+            case .success(let followers):
+                if followers.count < 100 {self.hasMoreFollowers = false}
+                self.followers.append(contentsOf: followers)
+                if self.followers.isEmpty{
+                    let message = "This User does not have any followers."
+                    DispatchQueue.main.async { self.showEmptyStateView(with: message, in: self.view) }
+                    return
+                                    }
+                self.updateData()
+                
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "Ok")
+            }
+            
+        }
+        
+    }
     
     func configureDataSource() {
         dataSource = UICollectionViewDiffableDataSource<Section, Follower>(collectionView: collectionView, cellProvider: { (collectionView, indexPath, follower) -> UICollectionViewCell? in
@@ -95,7 +102,7 @@ class FollowerListVC: UIViewController {
         
     }
     
-
+    
 }
 
 extension FollowerListVC: UICollectionViewDelegate {
@@ -104,7 +111,7 @@ extension FollowerListVC: UICollectionViewDelegate {
         let contentHeight = scrollView.contentSize.height
         let height = scrollView.frame.size.height
         
-        if offsetY > contentHeight - height {
+        if (offsetY > contentHeight - height) && !decelerate {
             guard hasMoreFollowers else { return }
             page += 1
             getFollowers(username: username, page: page)
