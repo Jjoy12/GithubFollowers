927832b513f39e67e0ab4fa8f935cfea5c26fe6b
Jjoy12 <Jerrionjoy@gmail.com>
2021-01-06 08:54:41 -0600

    refactor

---
+++ GithubFollowers/Custom_Views/Views/GFEmptyState.swift
@@ -0,0 +1,20 @@
+//
+//  GFEmptyState.swift
+//  GithubFollowers
+//
+//  Created by Jerrion Joy on 1/6/21.
+//
+
+import UIKit
+
+class GFEmptyState: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
