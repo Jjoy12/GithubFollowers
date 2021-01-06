466a825ac0cea713c11e734e0ffc825a45ceb48f
Jjoy12 <Jerrionjoy@gmail.com>
2021-01-06 08:59:27 -0600

    Configuring dismiss and empty state

--- GithubFollowers/Extensions/UIViewController+ext.swift
+++ GithubFollowers/Extensions/UIViewController+ext.swift
@@ -7,6 +7,9 @@
 
 import UIKit
 
+
+fileprivate var containerView: UIView!
+
 extension UIViewController{
     
     func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String){
@@ -16,5 +19,43 @@ extension UIViewController{
             alertVC.modalTransitionStyle = .flipHorizontal
             self.present(alertVC, animated: true)
         }
+        
+    }
+    
+    
+    func showLoadingView() {
+        containerView = UIView(frame: view.bounds)
+        view.addSubview(containerView)
+        
+        containerView.backgroundColor = .systemBackground
+        containerView.alpha = 0
+// Subtle animation
+        UIView.animate(withDuration: 0.25) { containerView.alpha = 0.8 }
+        
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        containerView.addSubview(activityIndicator)
+// Constraints
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            
+            activityIndicator.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            activityIndicator.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        activityIndicator.startAnimating()
+    }
+    
+    func dismissLoadingView() {
+        DispatchQueue.main.async {
+            containerView.removeFromSuperview()
+            containerView = nil
+        }
+        
+    }
+    
+    func showEmptyStateView(with message: String, in view: UIView) {
+        let emptyStateView = GFEmptyState(message: message)
+        emptyStateView.frame = view.bounds
+        view.addSubview(emptyStateView)
     }
 }
